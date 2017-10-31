b685eda9894bc9bd03754db6542182360a72e134
a-ammor <2003500@talnet.nl>
2017-10-31 08:33:24 +0100

    Website update

---
+++ header.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="standard.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<title>Anwars index</title>
+	<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section -->
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Menu</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="../">Home</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">HTML<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="Database/register.php">Database</a></li>
+							<li><a href="Slice&dice/Dice.html">Dice Game</a></li>
+							<li><a href="Opdrachtenhtml/opdracht01/opdracht1.php">Opdracht 1</a></li>
+							<li><a href="Opdrachtenhtml/opdracht02/opdracht2.php">Opdracht 2</a></li>
+							<li><a href="Opdrachtenhtml/opdracht03/opdracht3.html"> Opdracht 3 </a></li>
+							<li><a href="Opdrachtenhtml/opdracht04/opdracht4.html"> Opdracht 4 </a></li>
+							<li><a href="Opdrachtenhtml/opdracht05/opdracht5.html"> Opdracht 5 </a></li>
+							<li><a href="Opdrachtenhtml/opdracht06/opdracht6.html"> Opdracht 6 </a></li>
+							<li><a href="Opdrachtenhtml/opdracht07/opdracht7.html"> Opdracht 7 </a></li>
+							<li><a href="Opdrachtenhtml/opdracht08/opdracht8.php"> Opdracht 8 </a></li>
+							<li><a href="Opdrachtenhtml/opdracht09/opdracht9.php"> Opdracht 9 </a></li>
+							<li><a href="Opdrachtenhtml/opdracht10.php"> Opdracht 10 </a></li>
+							<li><a href="Opdrachtenhtml/Project/contact.html"> Project </a></li>
+							<li><a href="Opdrachtenhtml/bierhier/home.html"> Project Final </a></li>
+							<li><a href="Opdrachtenbootstrap/index.html"> Bootstrap </a></li>
+							<li><a href="Challenge/challenge.html"> Challenge </a></li>
+						</ul>
+					</li>
+
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PHP<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="opdrachtenphp/phpbegincursus/main.php"> Kerstboom </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/zwemclubs.php"> Zwemclubs </a> </li>
+							<li><a href="opdrachtenphp/phpbegincursus/kapperszaak.php"> Kapperszaak </a> </li>
+							<li><a href="opdrachtenphp/phpbegincursus/functies.php"> Functies </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/include/main.php"> Includes </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/Dynamisch_Form/dynamisch.php"> Dynamish </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/beasties_form/beasties.php"> Beasties </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/loginFormulier.php"> Login Formulier </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/login/login.php"> Login Pagina </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/class_method/index.php"> Object Oriented Programming </a></li>
+							<li><a href="opdrachtenphp/phpbegincursus/monkeybusiness/monkeybusiness.php"> Monkey Business </a></li>
+						</ul>
+					</li>
+
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projecten?<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Link een</a></li>
+							<li><a href="#">Link twee</a></li>
+							<li><a href="#">Link drie</a></li>
+							<li><a href="#">Link vier</a></li>
+						</ul>
+					</li>
+
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Databases?<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Link een</a></li>
+							<li><a href="#">Link twee</a></li>
+							<li><a href="#">Link drie</a></li>
+							<li><a href="#">Link vier</a></li>
+						</ul>
+					</li>
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pauze<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="https://www.youtube.com/">Youtube</a></li>
+							<li><a href="http://www.surrenderat20.net/">Surrenderat20</a></li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Login</a></li>
+					<li><a href="#">register</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+
+<center>
+  <p>Have fun!</p>
+
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+    <li><a href="http://wowslider.net"><img src="data1/images/classroom.jpg" alt="html5 slideshow" title="Classroom" id="wows1_0"/></a></li>
+    <li><img src="data1/images/rooster.png" alt="Rooster" title="Rooster" id="wows1_1"/></li>
+  </ul></div>
+  <div class="ws_bullets"><div>
+    <a href="#" title="Classroom"><span><img src="data1/tooltips/classroom.jpg" alt="Classroom"/>1</span></a>
+    <a href="#" title="Rooster"><span><img src="data1/tooltips/rooster.png" alt="Rooster"/>2</span></a>
+  </div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">css image slider</a> by WOWSlider.com v8.8</div>
+<div class="ws_shadow"></div>
+</div>  
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+
+
+</center>
+
+<?php include 'footer.php'; ?>
